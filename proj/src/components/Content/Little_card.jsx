856ab622716a7a2461d98c1afb30858838124a57
vadim1011 <vadim.nik999@gmail.com>
2025-10-28 13:24:43 +0200

    sneakers

---
+++ proj/src/components/Content/Little_card.jsx
@@ -0,0 +1,22 @@
+
+
+const Little_card = ({title, price, rating, img}) => {
+  return (
+    <div>
+        <img src={img} alt="" />
+        <div>
+            <h5>{title}</h5>
+            <ul>
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </ul>
+            <p>{price}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Little_card
--- proj/src/components/Content/SideBar.jsx
+++ proj/src/components/Content/SideBar.jsx
@@ -1,8 +1,44 @@
+import Little_card from "./Little_card"
 
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <div>
+      <form action="">
+        <input type="text" />
+      </form>
+      <div>
+        <h4></h4>
+        <ul>
+          <li>
+            <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
+            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
+            <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
+            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
+            <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
+            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
+             <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
+            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
+             <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
+            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
+          </li>
+        </ul>
+        <ul>
+          <li>
+             <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
+            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
+             <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
+            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
+             <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
+            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
+             <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
+            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
+             <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
+            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
