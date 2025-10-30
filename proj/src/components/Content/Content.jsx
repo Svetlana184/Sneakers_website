515c1d08c76a0d7264edecaa5edd3dd34b33dc63
Svetlana184 <sveta.kgd.39@gmail.com>
2025-10-30 19:01:35 +0200

    all but no arrays

--- proj/src/components/Content/Content.jsx
+++ proj/src/components/Content/Content.jsx
@@ -2,6 +2,7 @@ import Card from './Card'
 import styles from './Content.module.scss'
 import SideBar from './SideBar'
 import {cards} from "../../data.js"
+import arrow from '../../assets/footer_icons/arrow.png'
 
 
 const Content = () => {
@@ -21,16 +22,26 @@ const Content = () => {
                 <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card1.png" price="$25"/></li>
                 <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card2.png" price="$25"/></li>
                 <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card3.png" price="$25"/></li>
-                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card1.png" price="$25"/></li>
-                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card2.png" price="$25"/></li>
-                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card3.png" price="$25"/></li>
-                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card1.png" price="$25"/></li>
-                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card2.png" price="$25"/></li>
-                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card3.png" price="$25"/></li>
-                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card1.png" price="$25"/></li>
-                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card2.png" price="$25"/></li>
-                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card3.png" price="$25"/></li>
+                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card4.png" price="$25"/></li>
+                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card5.png" price="$25"/></li>
+                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card6.png" price="$25"/></li>
+                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card7.png" price="$25"/></li>
+                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card8.png" price="$25"/></li>
+                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card9.png" price="$25"/></li>
+                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card10.png" price="$25"/></li>
+                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card11.png" price="$25"/></li>
+                <li><Card title="Vel vestibulum elit tuvel euqen." img="../../src/assets/card_img/card12.png" price="$25"/></li>
             </ul>
+            <div className={styles.pages}>
+              <div className={styles.pages_btns}>
+                  <button className={styles.btn_red}>1</button>
+                  <button className={styles.btn_white}>2</button>
+              </div>
+              <button>
+                <p>next</p>
+                <img src={arrow} alt="" />
+              </button>
+            </div>
         </div>
        
         <SideBar/>
--- proj/src/components/Content/SideBar.jsx
+++ proj/src/components/Content/SideBar.jsx
@@ -1,40 +1,57 @@
 import Little_card from "./Little_card"
-
+import styles from './Content.module.scss'
 
 const SideBar = () => {
   return (
-    <div>
-      <form action="">
-        <input type="text" />
+    <div className={styles.sidebar_content}>
+      <form className={styles.search}>
+        <input type="text" placeholder="  Search Models" />
       </form>
       <div>
-        <h4></h4>
+        <h4>Top Best Models</h4>
         <ul>
           <li>
-            <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
-            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
-            <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
-            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
-            <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
-            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
              <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
-            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
+            price="$15" img="../../src/assets/card_img/min1.png" rating="5"/>
+          </li>
+           <li>
              <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
-            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
+            price="$20" img="../../src/assets/card_img/min2.png" rating="5"/>
+          </li>
+          <li>
+             <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
+            price="$25" img="../../src/assets/card_img/min3.png" rating="4"/>
+          </li>
+          <li>
+             <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
+            price="$10" img="../../src/assets/card_img/min4.png" rating="4"/>
+          </li>
+          <li>
+             <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
+            price="$15" img="../../src/assets/card_img/min5.png" rating="4"/>
           </li>
         </ul>
+        <h4>You May Like</h4>
         <ul>
+            <li>
+             <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
+            price="$10" img="../../src/assets/card_img/min6.png" />
+          </li>
+           <li>
+             <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
+            price="$20" img="../../src/assets/card_img/min7.png" />
+          </li>
           <li>
              <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
-            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
+            price="$15" img="../../src/assets/card_img/min8.png" />
+          </li>
+          <li>
              <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
-            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
+            price="$35" img="../../src/assets/card_img/min9.png" />
+          </li>
+          <li>
              <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
-            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
-             <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
-            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
-             <Little_card title="Primis in faucibus aenean laoreet rhoncus ipsum eget." 
-            price="$15" img="../../src/assets/card_img/card1.png" rating="5"/>
+            price="$20" img="../../src/assets/card_img/min10.png" />
           </li>
         </ul>
       </div>
